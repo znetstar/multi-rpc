537ffac20886410a1a06239a8e340048c00465e7
Zachary Boyd <zachary@zacharyboyd.nyc>
2021-07-20 22:17:28 -0400

    Uses `@etomon/encode-tools` for serializing messages, which enables the usage of `cbor` and `bson` serialization formats, in addition to the existing `msgpack` and `json`.

--- src/JSONSerializer.ts
+++ src/JSONSerializer.ts
@@ -1,39 +1,16 @@
-import { Serializer, ParseError, Response, Message } from "multi-rpc-common";
+import {EncodeToolsSerializer} from "multi-rpc-common";
+import {SerializationFormat} from "@etomon/encode-tools/lib/EncodeTools";
+
 const { TextDecoder } = require("text-encoding");
 const textDecoder = new TextDecoder();
 
 /**
  * Represents a serializer that can serialize and deserialize JSON.
  */
-export default class JSONSerializer extends Serializer {
-    /**
-     * The content type of the serialized object.
-     */
-    public get content_type(): string { return "application/json"; }
-
-    /**
-     * Serializes a message to JSON.
-     * @param object - Message to serialize.
-     */
-    serialize(object: Message): string {
-        return JSON.stringify(object.serialize ? object.serialize() : object);
-    }
-
-    /**
-     * Deserializes JSON to a message.
-     * @param data - Either a JSON string or binary containing JSON.
-     */
-    deserialize(data: Uint8Array|string){
-        if (data instanceof Uint8Array)
-            data = textDecoder.decode(data);
-        
-        let jsonObject;
-        try {
-            jsonObject = JSON.parse(<string>data);
-        } catch (error) {
-            throw new ParseError(error);
-        }
-
-        return super.deserialize(jsonObject);
+export default class JSONSerializer extends EncodeToolsSerializer {
+    constructor() {
+      super({
+        serializationFormat: SerializationFormat.json
+      });
     }
 }
